27ab947f67fb3faac14e7728f8e89ca4d7d42848
Sulav Neupane <neupsulav@gmail.com>
2023-03-22 18:12:48 +0545

    Added sorting and pagination

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,9 @@
+const model = require("../models/model");
+
 const getAllDataStatic = async (req, res) => {
   try {
-    res.status(200).json({ msg: "Getting all static data" });
+    const products = await model.find({}).select("name price");
+    res.status(200).send({ products, Hits: products.length });
   } catch (error) {
     res.status(500).send(error);
   }
@@ -8,7 +11,41 @@ const getAllDataStatic = async (req, res) => {
 
 const getAllData = async (req, res) => {
   try {
-    res.status(200).json({ msg: "Getting all data" });
+    const { featured, company, name, sort, fields } = req.query;
+    const queryObjects = {};
+
+    if (featured) {
+      queryObjects.featured = featured === true ? true : false;
+    }
+    if (company) {
+      queryObjects.company = company;
+    }
+    if (name) {
+      queryObjects.name = { $regex: name, $options: "i" };
+    }
+
+    let result = model.find(queryObjects);
+
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createdAt");
+    }
+
+    if (fields) {
+      const fieldList = fields.split(",").join(" ");
+      result = result.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
+    res.status(200).send({ products, Hits: products.length });
   } catch (error) {
     res.status(500).send(error);
   }
